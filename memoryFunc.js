3dbe5b98fecbd69f5f7fb3791076ea81d9d884e0
Maria Guerra <maguerra96@icloud.com>
2023-09-18 23:34:31 -0400

    new files

---
+++ memoryFunc.js
@@ -0,0 +1,153 @@
+const moves = document.getElementById("moves-count");
+const timeValue = document.getElementById('time');
+const startButton = document.getElementById("start");
+const stopButton = document.getElementById("stop");
+const gameContainer = document.querySelector(".game-container");
+const result = document.getElementById("result");
+const controls = document.querySelector(".controls-container");
+let cards;
+let interval;
+let firstCard = false;
+let secondCard = false;
+
+const items = [
+  { name: "bus", image: "bus.png" },
+  { name: "motocycle", image: "motocycle.png" },
+  { name: "car", image: "car.png" },
+  { name: "rocket", image: "rocket.png" },
+  { name: "camper", image: "camper.png" },
+  { name: "airplane", image: "airplane.png" },
+  { name: "train", image: "train.png" },
+  { name: "excavator", image: "excavator.png" },
+  { name: "pickup", image: "pickup.png" },
+  { name: "caravan", image: "caravan.png" },
+  { name: "delivery", image: "delivery.png" },
+  { name: "limo", image: "limo.png" },
+];
+
+let seconds = 0,
+minutes = 0;
+
+let movesCount = 0,
+  winCount = 0;
+
+const timeGenerator = () => {
+  seconds += 1;
+  if (seconds >= 60) {
+    minutes += 1;
+    seconds = 0;
+  }
+let secondsValue = seconds < 10 ? `0${seconds}` : seconds;
+let minutesValue = minutes < 10 ? `0${minutes}` : minutes;
+timeValue.innerHTML = `<span>Time:</span>${minutesValue}:${secondsValue}`;
+};
+
+const movesCounter = () => {
+  movesCount += 1;
+  moves.innerHTML = `<div>Moves:</div>${movesCount}`;
+};
+
+const generateRandom = (size = 4) => {
+let tempArray = [...items];
+let cardValues = [];
+size = (size * size) / 2;
+
+for (let i = 0; i < size; i++) {
+const randomIndex = Math.floor(Math.random() * tempArray.length);
+cardValues.push(tempArray[randomIndex]);
+tempArray.splice(randomIndex, 1);
+}
+  return cardValues;
+};
+
+const matrixGenerator = (cardValues, size = 4) => {
+ gameContainer.innerHTML = "";
+  cardValues = [...cardValues, ...cardValues];
+  cardValues.sort(() => Math.random() - 0.5);
+
+for (let i = 0; i < size * size; i++) {
+gameContainer.innerHTML += `
+  <div class="card-container" data-card-value="${cardValues[i].name}">
+    <div class="card-before">&#128516</div>
+    <div class="card-after">
+    <img src="${cardValues[i].image}" class="image"/></div>
+  </div>`;
+}
+  
+gameContainer.style.gridTemplateColumns = `repeat(${size},auto)`;
+
+cards = document.querySelectorAll(".card-container");
+cards.forEach((card) => {
+card.addEventListener("click", () => {
+    
+if (!card.classList.contains("matched")) {
+  card.classList.add("flipped");
+      
+if (!firstCard) {
+  firstCard = card;
+  firstCardValue = card.getAttribute("data-card-value");
+  } else {
+  movesCounter();
+  secondCard = card;
+      
+let secondCardValue = card.getAttribute("data-card-value");
+
+if (firstCardValue == secondCardValue) { 
+firstCard.classList.add("matched");
+secondCard.classList.add("matched");
+
+firstCard = false;
+setScore(currentScore + 1);
+           
+if (winCount == Math.floor(cardValues.length / 2)) {
+  result.innerHTML = `<h2>You Win!</h2>
+  <h4>Moves: ${movesCount}</h4>`;
+  stopGame();
+}
+  } else {
+    let [tempFirst, tempSecond] = [firstCard, secondCard];
+    firstCard = false;
+    secondCard = false;
+    let delay = setTimeout(() => {
+    tempFirst.classList.remove("flipped");
+    tempSecond.classList.remove("flipped");
+      }, 1000);
+    }
+  }
+}
+});
+});
+};
+
+startButton.addEventListener("click", () => {
+  movesCount = 0;
+  seconds = 0;
+  minutes = 0;
+ 
+  controls.classList.add("hide");
+  stopButton.classList.remove("hide");
+  startButton.classList.add("hide");
+ 
+  interval = setInterval(timeGenerator, 1000);
+
+  moves.innerHTML = `<span>Moves:</span> ${movesCount}`;
+  initializer();
+});
+
+stopButton.addEventListener(
+  "click",
+  (stopGame = () => {
+    controls.classList.remove("hide");
+    stopButton.classList.add("hide");
+    startButton.classList.remove("hide");
+    clearInterval(interval);
+  })
+);
+
+const initializer = () => {
+  result.innerText = "";
+  winCount = 0;
+  let cardValues = generateRandom();
+  console.log(cardValues);
+  matrixGenerator(cardValues);
+};
